50c817add768bacbe6363dd3b7f84ac3b1559d4a
luoyebing <luoyebing@meituan.com>
2018-05-14 23:26:37 +0800

    初始化项目框架

---
+++ src/test/java/com/it/web/CurdTest.java
@@ -0,0 +1,13 @@
+package com.it.web;
+
+/**
+ * Created with IDEA
+ * author:luoyebing
+ * Date:2018/5/14
+ * Time:下午11:09
+ */
+public class CurdTest {
+    public static void main(){
+        System.out.println("helloword");
+    }
+}
